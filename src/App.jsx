7dbafb99250723d7ab3f817a22573b077a17862d
Henrique Lima <barrosohenriquelima@gmail.com>
2022-11-07 18:36:18 -0300

    funcionalidades post

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,37 @@ import { Post } from "../src/components/Post";
 import styles from "./app.module.css";
 import "./global.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://avatars.githubusercontent.com/u/59373464?v=4",
+      name: "Henrique Lima",
+      role: "Software Engineer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galera, tudo bem?" },
+      { type: "paragraph", content: "Este é um novo projeto React" },
+      { type: "link", content: "github.com/Henrique0896" },
+    ],
+    publishedAt: new Date("2022-10-31T10:00:00Z"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "Educator",
+    },
+    content: [
+      { type: "paragraph", content: "Faaala Dev" },
+      { type: "paragraph", content: "Veja estes projetos a baixo que massa" },
+      { type: "link", content: "github.com/Henrique0896" },
+    ],
+    publishedAt: new Date("2022-10-31T12:00:00Z"),
+  },
+];
+
 function App() {
   return (
     <div>
@@ -12,8 +43,15 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {posts.map((post) => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,33 +1,53 @@
 import styles from "./Post.module.css";
+import { format, formatDistanceToNow } from "date-fns";
+import ptBr from "date-fns/locale/pt-BR";
+
 import { Comment } from "./Comment.jsx";
 import { Avatar } from "./Avatar";
 
-export function Post() {
+export function Post({ author, publishedAt, content }) {
+  const publishedDateFormatted = format(
+    publishedAt,
+    "d 'de' LLLL 'às' HH:mm'h'",
+    { locale: ptBr }
+  );
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBr,
+    addSuffix: true,
+  });
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar
-            src="https://avatars.githubusercontent.com/u/59373464?v=4"
-          />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
             <strong>Henrique Lima</strong>
             <span>Web developer</span>
           </div>
         </div>
 
-        <time title="11 de Maio às 08:13h" dateTime="2022-05-11">
-          {" "}
-          Publicado há 1h
+        <time
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
+        >
+          {publishedDateRelativeToNow}
         </time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala Turma!</p>
-        <p>Este é um novo projeto</p>
-        <p>
-          <a href="">#React</a>
-        </p>
+        {content.map((line) => {
+          if (line.type === "paragraph") {
+            return <p>{line.content}</p>;
+          } else if (line.type === "link") {
+            return (
+              <p>
+                <a href="#">{line.content}</a>
+              </p>
+            );
+          }
+        })}
       </div>
 
       <form className={styles.commentForm}>
